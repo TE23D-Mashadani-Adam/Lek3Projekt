21eac25b7ec2df599c4b2092e491ca3449d55e54
Adam Mashadani <adam.mashadani@elev.ga.ntig.se>
2025-09-16 13:02:32 +0200

    Lite effektivare system

--- src/App.java
+++ src/App.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        int[] passagerare = new int[20];
+        Passagerare[] passagerare = new Passagerare[20];
 
-        int pris = 299;
+        int vuxenPris = 299;
+        int barnPris = 149;
+        int pris = 0;
+
         int vinst = 0;
         boolean avsluta = false;
 
-        String[] meny = { "Lägg till en passagerare", "Skriv ut lediga platser", "Beräkna vinst",
-                "Avsluta programmet" };
+        String[] meny = { "Lägg till en passagerare", "Skriv ut lediga platser", "Hitta en bokning", "Beräkna vinst",
+                "Avboka en plats", "Avsluta programmet" };
 
         while (true) {
-            for (int i = 0; i < meny.length; i++) {
-                System.out.println(i + 1 + ". " + meny[i]);
-            }
+            ShowMenu(meny);
             int val = 0;
 
             while (true) {
@@ -31,6 +32,26 @@ public class App {
 
             switch (val) {
                 case 1:
+                    System.out.println("Ange ålder:");
+                    int ålder = 0;
+                    while (true) {
+                        try {
+                            ålder = scanner.nextInt();
+                            if (ålder < 18) {
+                                pris = barnPris;
+                            } else {
+                                pris = vuxenPris;
+                            }
+                            break;
+                        } catch (InputMismatchException a) {
+                            System.out.println("Skriv in siffror tack!");
+                            scanner.nextLine();
+                        }
+                    }
+
+                    System.out.println("Ange namn:");
+                    String namn = scanner.next();
+
                     System.out.println("Skriv personnummer (ååååmmdd):");
                     int perNummer = 0;
                     while (true) {
@@ -46,15 +67,48 @@ public class App {
                             scanner.nextLine();
                         }
                     }
-                    vinst = LäggTill(passagerare, perNummer, vinst, pris);
-                    break;
 
+                    vinst = LäggTill(passagerare, perNummer, vinst, pris, namn, ålder);
+                    break;
                 case 2:
+                    System.out.println("\n" + "Passagerare: ");
                     SkrivUt(passagerare);
+                    System.out.println("");
                     break;
 
                 case 3:
+                    System.out.println("Skriv in din personnummer tack");
+                    int bokadPerNummer = 0;
+                    while (true) {
+                        try {
+                            bokadPerNummer = scanner.nextInt();
+                            break;
+                        } catch (InputMismatchException a) {
+                            System.out.println("Försök igen");
+                            scanner.nextLine();
+                        }
+                    }
+                    HittaBokning(passagerare, bokadPerNummer);
+
+                    break;
+
+                case 4:
                     System.out.println(BeräknaVinst(vinst));
+                    break;
+
+                case 5:
+                    System.out.println("Skriv in din personnummer tack");
+                    int avbokningPerNummer = 0;
+                    while (true) {
+                        try {
+                            avbokningPerNummer = scanner.nextInt();
+                            break;
+                        } catch (InputMismatchException a) {
+                            System.out.println("Försök igen");
+                            scanner.nextLine();
+                        }
+                    }
+                    vinst = Avboka(passagerare, avbokningPerNummer, vinst);
                     break;
                 default:
                     avsluta = true;
@@ -68,11 +122,24 @@ public class App {
 
     }
 
-    static int LäggTill(int[] passagerare, int perNummer, int vinst, int pris) {
+    static void ShowMenu(String[] meny) {
+        for (int i = 0; i < meny.length; i++) {
+            System.out.println(i + 1 + ". " + meny[i]);
+        }
+    }
+
+    static int LäggTill(Passagerare[] passagerare, int perNummer, int vinst, int pris, String namn, int ålder) {
+
+        Passagerare nyPassagerare = new Passagerare();
+        nyPassagerare.namn = namn;
+        nyPassagerare.personnummer = perNummer;
+        nyPassagerare.ålder = ålder;
+        nyPassagerare.prisKlass = pris;
+
         for (int i = 0; i < passagerare.length; i++) {
-            if (passagerare[i] == 0) {
-                passagerare[i] = perNummer;
-                vinst += pris;
+            if (passagerare[i] == null) {
+                passagerare[i] = nyPassagerare;
+                vinst += nyPassagerare.prisKlass;
                 break;
             }
             if (i == passagerare.length - 1) {
@@ -82,10 +149,43 @@ public class App {
         return vinst;
     }
 
-    static void SkrivUt(int[] passagerare) {
+    static void SkrivUt(Passagerare[] passagerare) {
         for (int i = 0; i < passagerare.length; i++) {
-            System.out.println(i + 1 + ". " + passagerare[i]);
+            if (passagerare[i] != null) {
+                System.out.println(i + 1 + ". " + passagerare[i].namn + "Per. nummer: " + passagerare[i].personnummer);
+            } else {
+                System.out.println(i + 1 + ". Ledig plats");
+            }
         }
+    }
+
+    static void HittaBokning(Passagerare[] passagerare, int perNummer) {
+        for (int i = 0; i < passagerare.length; i++) {
+            if (passagerare[i].personnummer == perNummer) {
+                System.out.println("Du har sittplatsen: " + i + 1 + " på bussen");
+                break;
+            }
+            if (i == passagerare.length - 1) {
+                System.out.println("Du har ingen sittplats på bussen, du är alltid välkommen att boka hos oss!");
+                break;
+            }
+        }
+
+    }
+
+    static int Avboka(Passagerare[] passagerare, int perNummer, int vinst) {
+        for (int i = 0; i < passagerare.length; i++) {
+            if (perNummer == passagerare[i].personnummer) {
+                vinst -= passagerare[i].prisKlass;
+                passagerare[i] = null;
+                System.out.println("Avbokning genomförd! Välkommen åter");
+                break;
+            }
+            if (i == passagerare.length - 1) {
+                System.out.println("Du har ingen plats registrerat med denna personnummer på bussen");
+            }
+        }
+        return vinst;
     }
 
     static int BeräknaVinst(int vinst) {
---
+++ src/Passagerare.java
@@ -0,0 +1,8 @@
+public class Passagerare {
+
+    //Data värden
+    String namn;
+    int ålder;
+    int personnummer;
+    int prisKlass;
+}
